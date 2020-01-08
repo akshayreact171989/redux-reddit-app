1be5e6007a746d29a961d75b0023334943654c68
akshayreact171989 <akshayindore@gmail.com>
2020-01-08 19:15:39 +0530

    Updates

---
+++ precache-manifest.97bb8ef4cdbba9bb7063f27637451f70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd055e5a38fed07b2b76969310a60d48",
+    "url": "/redux-reddit-app/index.html"
+  },
+  {
+    "revision": "ee63105ccfde521a9c09",
+    "url": "/redux-reddit-app/static/css/main.6470b25b.chunk.css"
+  },
+  {
+    "revision": "2dd8398b0cfa1f274eb8",
+    "url": "/redux-reddit-app/static/js/2.3560cf2a.chunk.js"
+  },
+  {
+    "revision": "4544b2b45c9cfea0feb3c391ccb163d8",
+    "url": "/redux-reddit-app/static/js/2.3560cf2a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee63105ccfde521a9c09",
+    "url": "/redux-reddit-app/static/js/main.057bf6e6.chunk.js"
+  },
+  {
+    "revision": "12a62f8be46e50350b82",
+    "url": "/redux-reddit-app/static/js/runtime-main.46a34726.js"
+  }
+]);
